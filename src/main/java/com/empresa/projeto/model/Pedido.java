eddb56e109bb9f4e29ba384fd2d50322badfeb48
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-17 17:22:51 -0300

    Usando JPA puro

--- src/main/java/com/empresa/projeto/model/Pedido.java
+++ src/main/java/com/empresa/projeto/model/Pedido.java
@@ -1,10 +1,18 @@
 package com.empresa.projeto.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
 public class Pedido {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nomeProduto;
     private BigDecimal valorNegociado;
     private LocalDate dataEntrega;
