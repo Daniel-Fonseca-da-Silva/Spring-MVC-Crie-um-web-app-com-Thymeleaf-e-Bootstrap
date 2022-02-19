4c5ab514e9e9f769d0ae0c1231fc3dd6a7911927
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-19 01:09:21 -0300

    Salvando pedido

--- src/main/java/com/empresa/projeto/controller/PedidoController.java
+++ src/main/java/com/empresa/projeto/controller/PedidoController.java
@@ -1,15 +1,30 @@
 package com.empresa.projeto.controller;
 
+import com.empresa.projeto.dto.RequisicaoNovoPedido;
+import com.empresa.projeto.model.Pedido;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/pedido")
 public class PedidoController {
 
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
     @GetMapping("/formulario")
     public String formulario() {
+        return "pedido/formulario";
+    }
+
+    @PostMapping("novo")
+    public String novo(RequisicaoNovoPedido requisicao) {
+
+        Pedido pedido = requisicao.toPedido();
+        pedidoRepository.save(pedido);
         return "pedido/formulario";
     }
 
