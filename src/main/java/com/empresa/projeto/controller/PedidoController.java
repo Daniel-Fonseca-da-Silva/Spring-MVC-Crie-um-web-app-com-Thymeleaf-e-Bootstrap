c9061036cf318c303ebd507b4c307a4b6fad214c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-18 23:38:12 -0300

    Formulário do pedido

---
+++ src/main/java/com/empresa/projeto/controller/PedidoController.java
@@ -0,0 +1,16 @@
+package com.empresa.projeto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pedido")
+public class PedidoController {
+
+    @GetMapping("/formulario")
+    public String formulario() {
+        return "pedido/formulario";
+    }
+
+}
