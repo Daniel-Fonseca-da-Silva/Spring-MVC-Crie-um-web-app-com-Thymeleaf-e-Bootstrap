d79ab171e3aad5f9b3b2a182e6ea00db5c1ef7d9
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-19 19:11:04 -0300

    Criando o template

--- src/main/java/com/empresa/projeto/controller/PedidoRepository.java
+++ src/main/java/com/empresa/projeto/controller/PedidoRepository.java
@@ -3,7 +3,6 @@ package com.empresa.projeto.controller;
 import com.empresa.projeto.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
