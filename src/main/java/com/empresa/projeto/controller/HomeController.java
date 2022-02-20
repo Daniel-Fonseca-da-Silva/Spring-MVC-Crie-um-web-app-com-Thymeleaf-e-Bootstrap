19e52e401d97fa1abc38df529ba6c387f9a4bcc5
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-20 00:50:05 -0300

    Rotas do status

--- src/main/java/com/empresa/projeto/controller/HomeController.java
+++ src/main/java/com/empresa/projeto/controller/HomeController.java
@@ -1,23 +1,44 @@
 package com.empresa.projeto.controller;
 
 import com.empresa.projeto.model.Pedido;
+import com.empresa.projeto.model.StatusPedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
+import java.util.Locale;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     @Autowired
     private PedidoRepository pedidoRepository;
 
-    @GetMapping("/home")
+
+    @GetMapping
     public String home(Model model) {
         List<Pedido> pedidos = pedidoRepository.findAll();
         model.addAttribute("pedidos", pedidos);
         return "home";
+    }
+
+    @GetMapping("/{status}")
+    public String statusPedido(@PathVariable("status") String status, Model model) {
+        List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.valueOf(status.toUpperCase(Locale.ROOT)));
+        model.addAttribute("pedidos", pedidos);
+        model.addAttribute("status", status);
+        return "home";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError() {
+        return "redirect:/home";
     }
 
 }
--- src/main/java/com/empresa/projeto/controller/PedidoRepository.java
+++ src/main/java/com/empresa/projeto/controller/PedidoRepository.java
@@ -1,10 +1,14 @@
 package com.empresa.projeto.controller;
 
 import com.empresa.projeto.model.Pedido;
+import com.empresa.projeto.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+    List<Pedido> findByStatus(StatusPedido status);
 }
